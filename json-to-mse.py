113be4be57e415bdea535159f41205394fbce94f
Fenhl <fenhl@fenhl.net>
2018-05-20 20:25:48 +0000

    Handle 404 errors from Scryfall API, not the image request

--- json-to-mse.py
+++ json-to-mse.py
@@ -494,16 +494,8 @@ class MSEDataFile:
                 if isinstance(artist, bytes):
                     artist = artist.decode('utf-8')
         elif use_scryfall:
-            scryfall_data = scryfall_request('https://api.scryfall.com/cards/named', params={'exact': card_info.name}).json()
-            artist = scryfall_data['artist']
-            if 'image_uris' in scryfall_data:
-                img_url = scryfall_data['image_uris']['art_crop']
-            elif 'card_faces' in scryfall_data:
-                img_url = more_itertools.one(filter(lambda face: face['name'] == card_info.name, scryfall_data['card_faces']))['image_uris']['art_crop']
-            else:
-                raise ValueError(f'Unexpexted format of Scryfall JSON data: {scryfall_data!r}')
             try:
-                response = scryfall_request(img_url)
+                scryfall_data = scryfall_request('https://api.scryfall.com/cards/named', params={'exact': card_info.name}).json()
             except requests.HTTPError as e:
                 if e.response.status_code == 404:
                     image = None
@@ -512,6 +504,14 @@ class MSEDataFile:
                 else:
                     raise
             else:
+                artist = scryfall_data['artist']
+                if 'image_uris' in scryfall_data:
+                    img_url = scryfall_data['image_uris']['art_crop']
+                elif 'card_faces' in scryfall_data:
+                    img_url = more_itertools.one(filter(lambda face: face['name'] == card_info.name, scryfall_data['card_faces']))['image_uris']['art_crop']
+                else:
+                    raise ValueError(f'Unexpexted format of Scryfall JSON data: {scryfall_data!r}')
+                response = scryfall_request(img_url)
                 with PIL.Image.open(io.BytesIO(response.content)) as img:
                     exif = piexif.load(img.info.get('exif', piexif.dump({})))
                     exif['0th'][piexif.ImageIFD.Artist] = artist.encode('utf-8')

25f15e931a46614ba16817d21b2be41b11b9d1d0
Fenhl <fenhl@fenhl.net>
2020-04-08 03:08:28 +0000

    Add log messages to release script

--- src/bin/release.rs
+++ src/bin/release.rs
@@ -33,9 +33,11 @@ fn release_client() -> Result<reqwest::blocking::Client, Error> { //TODO return 
 }
 
 fn main() -> Result<(), Error> {
+    eprintln!("creating reqwest client");
     let client = release_client()?;
     //TODO make sure working dir is clean and on master and up to date with remote and remote is up to date
     let repo = Repo::new("fenhl", "magic-set-generator");
+    eprintln!("checking version");
     if let Some(latest_release) = repo.latest_release(&client)? {
         let remote_version = latest_release.version()?;
         match version().cmp(&remote_version) {
@@ -44,12 +46,17 @@ fn main() -> Result<(), Error> {
             Greater => {}
         }
     }
+    eprintln!("waiting for Rust lock");
     let lock_dir = Path::new(&env::var_os("TEMP").ok_or(Error::MissingEnvar("TEMP"))?).join("syncbin-startup-rust.lock");
     let lock = DirLock::new_sync(&lock_dir);
+    eprintln!("updating Rust for x86_64");
     Command::new("rustup").arg("update").arg("stable").check("rustup")?;
+    eprintln!("updating Rust for i686");
     Command::new("rustup").arg("update").arg("stable-i686-pc-windows-msvc").check("rustup")?;
     drop(lock);
+    eprintln!("building msg-win64.exe");
     Command::new("cargo").arg("build").arg("--bin=msg-gui").arg("--release").check("cargo")?;
+    eprintln!("building msg-win32.exe");
     Command::new("cargo").arg("+stable-i686-pc-windows-msvc").arg("build").arg("--bin=msg-gui").arg("--release").arg("--target-dir=target-x86").check("cargo")?;
     let release_notes = {
         let mut release_notes_file = tempfile::Builder::new()
@@ -61,9 +68,13 @@ fn main() -> Result<(), Error> {
         release_notes_file.read_to_string(&mut buf).at(release_notes_file.path())?;
         buf
     };
+    eprintln!("creating release");
     let release = repo.create_release(&client, format!("Magic Set Generator {}", version()), format!("v{}", version()), release_notes)?;
+    eprintln!("uploading msg-win64.exe");
     repo.release_attach(&client, &release, "msg-win64.exe", "application/vnd.microsoft.portable-executable", File::open("target/release/msg-gui.exe").at("target/release/msg-gui.exe")?)?;
+    eprintln!("uploading msg-win32.exe");
     repo.release_attach(&client, &release, "msg-win32.exe", "application/vnd.microsoft.portable-executable", File::open("target-x86/release/msg-gui.exe").at("target-x86/release/msg-gui.exe")?)?;
+    eprintln!("publishing release");
     repo.publish_release(&client, release)?;
     Ok(())
 }
